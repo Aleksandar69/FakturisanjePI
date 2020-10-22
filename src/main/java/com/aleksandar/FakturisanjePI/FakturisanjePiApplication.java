8d5b8fbd4b3763e30d6a69533a2a6492e3769c81
Aleksandar Stojanovic <alestoj@hotmail.com>
2020-10-22 16:04:00 +0200

    Kreiran projekat uz pomoc Spring Initializr

---
+++ src/main/java/com/aleksandar/FakturisanjePI/FakturisanjePiApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandar.FakturisanjePI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturisanjePiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturisanjePiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksandar/FakturisanjePI/FakturisanjePiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandar.FakturisanjePI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturisanjePiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
